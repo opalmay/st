d4c2fdd895fa0dfae71d8bd27c9e112340182cf4
Opal <opal.mizrahi2@gmail.com>
2021-11-27 04:03:12 +0200

    conf

--- config.h
+++ config.h
@@ -5,8 +5,11 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono Nerd Font:pixelsize=14:antialias=true:autohint=true";
-static char *font2[] = { "JetBrains Mono Nerd Font:pixelsize=14:antialias=true:autohint=true" };
+// static char *font = "JetBrains Mono Nerd Font:pixelsize=18:antialias=true:autohint=true";
+// static char *font2[] = { "JetBrains Mono Nerd Font:pixelsize=18:antialias=true:autohint=true" };
+static char *font = "monospace:pixelsize=20:antialias=true:autohint=true";
+static char *font2[] = { "monospace:pixelsize=20:antialias=true:autohint=true" };
+
 static int borderpx = 2;
 
 /*
@@ -253,8 +256,8 @@ static Shortcut shortcuts[] = {
 	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
 	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
 	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
-	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
+	{ MODKEY,               XK_plus,        zoom,           {.f = +1} },
+	{ TERMMOD,              XK_minus,       zoom,           {.f = -1} },
 	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
